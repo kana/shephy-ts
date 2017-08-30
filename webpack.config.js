ffe0a5152059ffc283edd87f3efbd4afdcc4d749
Kana Natsuno <dev@whileimautomaton.net>
2017-08-30 23:49:17 +0900

    Add webpack.config.js skeleton

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
