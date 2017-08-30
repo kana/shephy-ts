19e0948fa7c766b2b12cfbdfe78570ae81baae1d
Kana Natsuno <dev@whileimautomaton.net>
2017-08-31 01:01:28 +0900

    Write some code to test vue

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-export function foo () {
-  alert('hi')
+import Vue, { ComponentOptions } from 'vue'
+
+interface VueApp extends Vue {
+  count: number
 }
+
+new Vue({
+  el: '#app',
+  template: '#app-template',
+  data: {
+    count: 0
+  },
+  methods: {
+    bump: function () {
+      this.count++
+    }
+  }
+} as ComponentOptions<VueApp>)
