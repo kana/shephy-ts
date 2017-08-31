cdd92dabdf10a26266390c4026fa7f2f56c29479
Kana Natsuno <dev@whileimautomaton.net>
2017-08-31 20:19:01 +0900

    Configure to use html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.ts',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
