ffe78ef6fedb3d057381cabc942e2c07ebec600b
Kana Natsuno <dev@whileimautomaton.net>
2017-08-31 20:37:10 +0900

    Add Root.vue

---
+++ src/Root.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Count: {{ count }}</p>
+    <button @click="bump">Bump!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    bump: function () {
+      this.count++
+    }
+  }
+}
+</script>
+vim: filetype=html
