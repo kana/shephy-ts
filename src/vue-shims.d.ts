bddb45ae3570d549d4645c7c488e4dae2dfe48b4
Kana Natsuno <dev@whileimautomaton.net>
2017-09-02 14:09:07 +0900

    Add type definitions for *.html/*.css

--- src/vue-shims.d.ts
+++ src/vue-shims.d.ts
@@ -2,3 +2,13 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+
+declare module '*.css' {
+  import Vue from 'vue'
+  interface WithRender {
+    <V extends Vue>(options: Vue.ComponentOptions<V>): Vue.ComponentOptions<V>
+    <V extends typeof Vue>(component: V): V
+  }
+  const withRender: WithRender
+  export = withRender
+}
