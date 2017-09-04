ec755d2fe4d935c8afe3c65342a94a75e650d8dd
Kana Natsuno <dev@whileimautomaton.net>
2017-09-04 22:51:29 +0900

    Add board.vue

---
+++ src/components/board.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :class="$style.container">
+    <dl>
+      <dt>Field:</dt>
+      <dd>
+        <span v-for="sheep in field" :class="$style.sheep">
+          {{ sheep }}
+        </span>
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<style>
+  .container {
+    background-color: #eee;
+  }
+
+  .sheep {
+    display: inline-block;
+    border: 1px solid gray;
+    background: #f9f9f9;
+    color: #000;
+    width: 63px;
+    height: 88px;
+  }
+
+  .sheep + .sheep {
+    margin-left: 1em;
+  }
+</style>
+
+<script>
+import Vue from 'vue'
+import template from './board.html?style=./board.css'
+
+interface IComponent extends Vue {
+  field: number[]
+}
+
+const component: Vue.ComponentOptions<IComponent> = template({
+  data () {
+    return {
+      field: [1, 3, 300, 10]
+    }
+  }
+})
+
+export default component
+</script>
+
+vim: filetype=html
