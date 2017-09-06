4625b4dc746750f6ba35d8f538b6acb956b8c2fe
Kana Natsuno <dev@whileimautomaton.net>
2017-09-07 00:43:40 +0900

    Separate Board representation

--- src/components/board.ts
+++ src/components/board.ts
@@ -1,13 +1,14 @@
+import Board from 'game/board'
 import Vue from 'vue'
 
 interface IComponent extends Vue {
-  field: number[]
+  board: Board
 }
 
 const component: Vue.ComponentOptions<IComponent> = {
   data () {
     return {
-      field: [1, 3, 300, 10, 100]
+      board: new Board()
     }
   }
 }
---
+++ src/game/board.ts
@@ -0,0 +1,7 @@
+export default class Board {
+  readonly field: number[]
+
+  constructor () {
+    this.field = [1, 10, 3, 1000, 300]
+  }
+}
