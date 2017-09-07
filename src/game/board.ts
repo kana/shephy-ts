8bf7e8ba6859d60056de9a8695d2136cb771bc19
Kana Natsuno <dev@whileimautomaton.net>
2017-09-08 00:42:29 +0900

    Add stocks

--- src/game/board.ts
+++ src/game/board.ts
@@ -1,7 +1,9 @@
+import { Rank } from 'game/rank'
 import Sheep from 'game/sheep'
 
 export default class Board {
   readonly field: Sheep[]
+  readonly stocks: Map<Rank, Sheep[]>
 
   constructor () {
     this.field = [
@@ -10,5 +12,10 @@ export default class Board {
       new Sheep(1000),
       new Sheep(30)
     ]
+
+    this.stocks = new Map()
+    this.stocks.set(1, [new Sheep(1), new Sheep(1)])
+    this.stocks.set(3, [new Sheep(3)])
+    this.stocks.set(10, [new Sheep(10), new Sheep(10), new Sheep(10)])
   }
 }
